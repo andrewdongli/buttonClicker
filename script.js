ad63de4d8e4177783c2265f46e6733ba3c8cdf40
Andrew Li <liandrewdong@gmail.com>
2021-08-23 12:49:44 -0700

    changed js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function login(element){
-    element.innerText ="Logout";
+    if (element.innerText == "Login"){
+        element.innerText ="Logout";
+    } else {
+        element.innerText = "Login";
+    }
+
 }
 
 function hide(element){
